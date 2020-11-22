2fa4cedd0fe8eae953153537ed60bd41b2234d24
Paul Cassell <pcassell3@gatech.edu>
2020-11-21 21:01:11 -0500

    Added the option of specify options file on command line.

--- start-proxy.rb
+++ start-proxy.rb
@@ -5,18 +5,27 @@ require_relative './fhir-proxy.rb'
 # Options for Rack are listed here
 # https://github.com/rack/rack/blob/master/lib/rack/server.rb
 #
+if ARGV[0].nil?
+  opts_file = 'proxy.yml'
+else
+  opts_file = ARGV[0]
+end
+
 begin
-  proxy_options = YAML.load_file('proxy.yml')
+  proxy_options = YAML.load_file(opts_file)
+  puts "Loading options from file #{opts_file}"
 rescue Errno::ENOENT
-  puts "Could not find file 'proxy.yml'"
-  puts 'Creating one with default values'
+  puts "Could not find file #{opts_file}"
+  puts "Creating #{opts_file} with default values"
   proxy_options = {
       Host: '0.0.0.0',
       Port: 9292,
       backend: 'https://r4.smarthealthit.org',
       db: 'fhir-transactions.db'
   }
-  File.write('proxy.yml', proxy_options.to_yaml)
+  File.write(opts_file, proxy_options.to_yaml)
+rescue Exception => e
+  puts e
 end
 Rack::Server.start(
   app: FHIRProxy.new(
