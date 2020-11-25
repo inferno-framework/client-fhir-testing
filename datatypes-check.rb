2dc495ce981438c5d2f7e114b883d7044899e8dd
qhu75 <qhu75@gatech.edu>
2020-11-25 00:30:24 -0500

    Fix data type checking

--- datatypes-check.rb
+++ datatypes-check.rb
@@ -1,11 +1,11 @@
 module CheckDatatypes
   def check_types(value, type)
     if type == "date"
-      /([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1]))?)?/.match?(value)
+      /([0-9]([0-9]([0-9][1-9]|[1-9]0)|[1-9]00)|[1-9]000)(-(0[1-9]|1[0-2])(-(0[1-9]|[1-2][0-9]|3[0-1]))?)?/.match(value).to_s == value
     elsif type == "token"
-      /[^\s]+(\s[^\s]+)*/.match?(value)
+      /[^\s]+(\s[^\s]+)*/.match(value).to_s == value
     elsif type == "string"
-      /[ \r\n\t\S]+/.match?(value)
+      /[ \r\n\t\S]+/.match(value).to_s == value
     else
       nil
     end
