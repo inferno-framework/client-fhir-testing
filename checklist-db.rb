ebc8ba0a6fb1fd8467d7eb6629765443a18e351d
qhu75 <qhu75@gatech.edu>
2020-11-30 20:23:07 -0500

    Add column annotation

--- checklist-db.rb
+++ checklist-db.rb
@@ -5,6 +5,18 @@ opts = YAML.load_file('proxy.yml')
 DataMapper.setup :default, "sqlite://#{Dir.pwd}/" + opts[:db]
 
 class CheckList
+  # id: serial number
+  # resource: FHIR resource / action
+  # request_type: read / vread / update / create / search-type
+  # search_param: Array of search parameters. nil if not 'search-type'.
+  # search_valid: boolean, whether search is valid (parameter in SHALL list and response status is 200)
+  # search_combination: 1 parameter => nil; >1 parameters & find in the SHALL list => SHALL combinations; >1 parameters & not in the SHALL list => []
+  # search_type: Array of boolean. whether each search value is valid for its data type. nil if not 'search-type'.
+  # present: The matched serial id in the interaction table.
+  # present_code: The matched interaction Code (SHALL/SHOULD/MAY) in the interaction table.
+  # request_id: The original request id in the request table from database.
+  # response_status: The response status from server in the response table from database.
+
   include DataMapper::Resource
 
   property :id, Serial
